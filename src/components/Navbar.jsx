4b6b2462a520268657af7f6e18c1a7032b48fe46
Alejandro Villegas <alejandro.villegas.m@gmail.com>
2024-10-24 15:04:43 -0300

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 const Navbar = () => {
   return (
@@ -15,22 +15,44 @@ const Navbar = () => {
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <a className="navbar-brand" href="#">Navbar</a>
+        <a className="navbar-brand" href="#">
+          Navbar
+        </a>
         <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">Home</a>
+              <a className="nav-link active" aria-current="page" href="#">
+                Inicio
+              </a>
+            </li>
+
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Quiénes somos
+              </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Link</a>
+              <a className="nav-link" href="#">
+                Documentación
+              </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+              <a className="nav-link" href="#">
+                Contacto
+              </a>
             </li>
           </ul>
+
           <form className="d-flex" role="search">
-            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-            <button className="btn btn-outline-success" type="submit">Search</button>
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="¿Qué necesitas?"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-success" type="submit">
+              Buscar
+            </button>
           </form>
         </div>
       </div>
